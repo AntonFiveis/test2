39a94dce7252aace4ef94cc3a2321ea4bee61fc2
Anton <antonfiveis@gmail.com>
2020-01-27 13:49:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {ans1:ans2},
+    optionalExpenses: {}
+};
+console.log(appData.budget);
+var ans1 = prompt("Введите обязательную статью расходов в этом месяце","");
+ans1 = prompt("Введите обязательную статью расходов в этом месяце","");
+var ans2 = prompt("Во сколько обойдется ? ","");
+ans2 = prompt("Во сколько обойдется ? ","");
+alert(appData.budget/30);
